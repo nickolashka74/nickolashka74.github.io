e515159c7b750d8ae87cd25cebc98e4dd5cef893
nickolashka <xy4u74@yandex.ru>
2021-08-28 17:03:33 +0500

    delete about me project

--- projects/about-me/js/script.js
+++
@@ -1,7 +0,0 @@
-const aboutLink = document.querySelector('.about__link'),
-	  aboutText = document.querySelector('.about__text');
-
-aboutLink.addEventListener('click', (e) => {
-	e.preventDefault();
-	aboutText.classList.toggle('show');
-});
